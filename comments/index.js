ed51ed1e1e735741e7f8a576f102361f116218e2
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2025-04-28 12:23:34 +0500

    updated

--- comments/index.js
+++ comments/index.js
@@ -8,18 +8,22 @@ app.use(cors());
 const commentsByPostId = {};
 
 app.get("/posts/:id/comments", (req, res) => {
-    res.send(commentsByPostId[req.params.id] || []);
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post("/posts/:id/comments", (req, res) => {
-  const commentId= randomBytes(4).toString("hex");
+  const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
-  comments.push({ id: commentId, content });
-    commentsByPostId[req.params.id] = comments;
-    res.status(201).send(comments);
+  comments.push({ id: commentId, content, postId: req.params.id }); 
+  commentsByPostId[req.params.id] = comments;
+  res.status(201).send(comments);
 });
 
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+  res.send({});
+});
 app.listen(4001, () => {
   console.log("Listening on port 4001");
 });
