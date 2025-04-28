2aa691530a767dd7b81560ae7a74604e85b4adc8
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2025-04-28 18:20:13 +0500

    complete project

--- posts/index.js
+++ posts/index.js
@@ -1,39 +1,46 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { randomBytes } from "crypto";
-import cors from "cors";
 import axios from "axios";
+import cors from "cors";
+
+import { randomBytes } from "crypto";
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
 const posts = {};
 
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", async(req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
+
   posts[id] = {
     id,
     title,
   };
-  await axios.post('http://localhost:4005/events', {
-    type: 'PostCreated',
+
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
     data: {
       id,
-      title
-    }
+      title,
+    },
   });
+
   res.status(201).send(posts[id]);
 });
 
 app.post("/events", (req, res) => {
   console.log("Received Event", req.body.type);
+
   res.send({});
 });
 
 app.listen(4000, () => {
-  console.log("Listening on port 4000");
+  console.log("Listening on 4000");
 });
--- query/index.js
+++ query/index.js
@@ -1,38 +1,72 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
+const handleEvent = (type, data) => {
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    // Initialize the post with an empty comments array
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+
+    if (post) {
+      post.comments.push({ id, content, status });
+    } else {
+      console.error(`Post with id ${postId} does not exist for CommentCreated event`);
+    }
+  }
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+
+    if (post) {
+      const comment = post.comments.find((comment) => comment.id === id);
+      if (comment) {
+        comment.status = status;
+        comment.content = content;
+      } else {
+        console.error(`Comment with id ${id} not found in post ${postId}`);
+      }
+    } else {
+      console.error(`Post with id ${postId} does not exist for CommentUpdated event`);
+    }
+  }
+};
+
 app.get("/posts", (req, res) => {
-    res.send(posts);
+  res.send(posts);
 });
 
 app.post("/events", (req, res) => {
-    const { type, data } = req.body;
-    
-    if (type === "PostCreated") {
-        const { id, title } = data;
-    
-        posts[id] = { id, title, comments: [] };
-    }
-    
-    if (type === "CommentCreated") {
-        const { id, content, postId } = data;
-    
-        const post = posts[postId];
-        post.comments.push({ id, content });
-    }
-    
-    console.log(posts);
-    
-    res.send({});
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
+
+  res.send({});
 });
 
-app.listen(4002, () => {
-  console.log("Query service listening on 4002");
+app.listen(4002, async () => {
+  console.log("Listening on 4002");
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
